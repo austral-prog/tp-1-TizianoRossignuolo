b637550427fb9cee66d7893840a082e1a2b06f56
TizianoRossignuolo <trossignuolo@mail.austral.edu.ar>
2025-03-21 10:10:35 -0300

    TP1_exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    suma_a_b= a + b
+    diferencia_a_b= a - b
+    producto_a_b= a * b
+    promedio_a_b= (a+b)/2
+    cociente_entero_a_b= int(a/b)
+    resto_a_b= a % b
+    cociente_a_b= a / b
+    print(suma_a_b)
+    print(diferencia_a_b)
+    print(producto_a_b)
+    print(promedio_a_b)
+    print(cociente_entero_a_b)
+    print(resto_a_b)
+    print(cociente_a_b)
